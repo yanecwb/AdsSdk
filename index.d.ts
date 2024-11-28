2df141c8ce635006cc3f1c5461b9d7b8e25ea67a
Noah <noah@mobikok.com>
2024-11-28 21:33:05 +0800

    wip:developing

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+type AdsType = 'adsGram' | 'openAd' | 'tonai';
+ 
--- index.ts
+++ index.ts
@@ -1,5 +1,45 @@
-import Format from "./src/format.ts";
-import Validate from "./src/validate.ts";
-import OpenAd from "./src/OpenAd/index.ts";
+import "./index.d.ts";
+import { load, onImpressions, getOpenAdBannerAds,generateOpenAd,getOpenAdTaskAds } from "./src/OpenAd/index.ts";
+function weightedRandom(arr: Array<AdsType>, weights: Array<number>): AdsType {
+  const totalWeight = weights.reduce((acc, weight) => acc + weight, 0);
+  const random = Math.random() * totalWeight;
+  let weightSum = 0;
 
-export { Format, Validate, OpenAd };
+  for (let i = 0; i < weights.length; i++) {
+    weightSum += weights[i];
+    if (random < weightSum) {
+      return arr[i];
+    }
+  }
+  return "adsGram";
+}
+
+const randomADS:AdsType = weightedRandom(["adsGram", "openAd", "tonai"], [5, 3, 2]);
+
+const AdsMap = {
+    adsGram:{
+
+    },
+    openAd:{
+        loadBannerAds:getOpenAdBannerAds
+    },
+    tonai:{}
+}
+const dynamicsEvent = (type: AdsType) => {
+  return onImpressions;
+};
+const getDynamicsEvent = (show, click) => {
+  (window as any).dynamicsEvent = { show, click };
+};
+
+const loadBannerAds = ()=> AdsMap['openAd'].loadBannerAds()
+
+export const index = {
+  loadBannerAds,
+  load,
+  dynamicsEvent,
+  generateOpenAd,
+  getOpenAdTaskAds,
+  getDynamicsEvent,
+};
+export default index;
